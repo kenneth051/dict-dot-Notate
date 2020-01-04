2fe76f1550f66fbcb3566feefa7356ad0c3e46b3
kenneth051 <kjdumba@gmail.com>
2020-01-04 23:05:52 +0300

    changes

--- dot_notate.py
+++ dot_notate.py
@@ -2,13 +2,26 @@ def dot_notate(obj, res, currentKey):
     for k, v in obj.items():
         newKey = currentKey + "." + k if currentKey else k
         if v and isinstance(v, dict):
-            objToStringDotNnotation(v,res,newKey)
+            dot_notate(v, res, newKey)
         else:
             res[newKey] = v
     return res
 
+
 def dict_dot_notate(obj):
     if isinstance(obj, dict):
         return dot_notate(obj, {}, currentKey=None)
-    raise TypeError("function dict_dot_notate expects a dictionary")
-    
+    raise TypeError("Function dict_dot_notate expects a dictionary")
+
+
+data = {
+    "obj": "obj",
+    "nested_once": {"first": "first"},
+    "nested_twice": {
+        "twice": {"inner_nest": {"second": "second"}}
+    },
+    "nested_thrice": {
+        "thrice": {"second_nest": {"tripple_nest": {"tripple": "thrice"}, "three": "three"}}
+    }
+}
+
