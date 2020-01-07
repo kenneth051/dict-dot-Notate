147b2edb83aa235b3e9f0af58a918a6bfccc55b3
kenneth051 <kjdumba@gmail.com>
2020-01-07 18:23:00 +0300

    add travis file

--- dot_notate/dot.py
+++ dot_notate/dot.py
@@ -4,7 +4,3 @@ def dict_dot_notate(obj):
     if isinstance(obj, dict):
         return dot_notate(obj, {}, currentKey=None)
     raise TypeError("Function dict_dot_notate expects a dictionary")
-
-
-
-# print(dict_dot_notate(data))
